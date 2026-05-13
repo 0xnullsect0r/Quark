23b1c86241e6278d8b2f23dcfc0514417fff05d1
Ari Cummings <ari@aricummings.com>
2026-05-13 15:04:47 -0700

    Update mod.rs

--- quark-core/src/data/mod.rs
+++ quark-core/src/data/mod.rs
@@ -3,7 +3,9 @@
 pub mod batch;
 pub mod loader;
 pub mod packing;
+pub mod pile;
 pub mod stats;
 
 pub use batch::{collate_batch, DataBatch};
 pub use loader::TextLoader;
+pub use pile::{detect_python, pile_components, start_pile_build, PileConfig, PileMessage};
